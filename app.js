7ad21591d0d71a252722e0963db8b119a6db7027
Rutvikbarot <rutvikbarot9737@gmail.com>
2023-03-07 12:51:47 +0530

    first

---
+++ app.js
@@ -0,0 +1,116 @@
+//jshint esversion:6
+
+require("dotenv").config();
+console.log(process.env);
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://Rutvik:97379737@cluster0.aismcf8.mongodb.net/secrets",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then((res) => {
+    console.log(`
+        Database connected successfully
+    `);
+  });
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(() => {
+      res.render("secrets");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username })
+    .then((foundUser) => {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    })
+    .catch((error) => {
+      //When there are errors We handle them here
+
+      console.log(err);
+      res.send(400, "Bad Request");
+    });
+});
+
+// app.post("/login", async (req, res) => {
+//   const username = req.body.username;
+//   const password = req.body.password;
+
+//   const user = await User.findOne(
+//     { email: username },
+//     function (err, foundUser) {
+//       console.log(foundUser);
+//       res.send(foundUser);
+//     }
+//   );
+// });
+
+app.listen(11000, function () {
+  console.log("server started on port 11000");
+});
+
+// if (err) {
+//   console.log(err);
+// } else {
+//   if (foundUser) {
+//     if (foundUser.password === password) {
+//       res.render("secrets");
+//     }
+//   }
+// }
